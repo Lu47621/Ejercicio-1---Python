88f14852e16222d37cd2960d58c4c2947c2a325f
lu <lulua47621@gmail.com>
2022-07-08 17:45:26 -0500

    Se agrego el ejercicio

---
+++ Ejercicio1.py
@@ -0,0 +1,33 @@
+##### Ejercicio 1 - Ficha de Datos #####
+Nombre = input("Ingresa tu nombre: ") ##Entrada de datos por parte del usuario
+N_telefono = input("Ingresa tu numero telefonico: ")
+Direccion = input("Ingresa tu direccion: ")
+Edad = input("Ingresa tu edad: ")
+Nacionalidad = input("Ingresa tu nacionalidad: ")
+Fecha_naci = input("Ingresa tu fecha de nacimiento: ")
+Altura = input("Ingresa tu altura: ")
+Peso = input("Ingresa tu peso: ")
+
+print(f"\n Nombre: {Nombre}")
+print(type(Nombre))##Muestra el tipo de entrada
+
+print(f"Numero de telefono: {N_telefono}")
+print(type(N_telefono))##Muestra el tipo de entrada
+
+print(f"Direccion: {Direccion}")
+print(type(Direccion))##Muestra el tipo de entrada
+
+print(f"Edad: {Edad} años")
+print(type(Edad))##Muestra el tipo de entrada
+
+print(f"Nacionalidad: {Nacionalidad}")
+print(type(Nacionalidad))##Muestra el tipo de entrada
+
+print(f"Fecha de nacimiento: {Fecha_naci}")
+print(type(Fecha_naci))##Muestra el tipo de entrada
+
+print(f"Altura: {Altura} cm")
+print(type(Altura))##Muestra el tipo de entrada
+
+print(f"Peso: {Peso} kg")
+print(type(Peso))##Muestra el tipo de entrada
