2bf3636d71124a6969a7d0795bc9009df4a1462e
Lu <94569496+Lu47621@users.noreply.github.com>
2022-07-19 22:16:03 -0500

    Add files via upload
    Ejercicio 2 - Listas
    Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.

---
+++ Ejercicio 2 - Listas.py
@@ -0,0 +1,8 @@
+#Ejercicio 2. Listas
+#Escribir un programa que almacene las asignaturas de un curso 
+#(por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista
+#y la muestre por pantalla
+
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+print(f"Las asignaturas de la lista son: {asignaturas}")
+
