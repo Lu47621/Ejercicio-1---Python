059f4836955c3d9be1e6c1638840f3cb2bebf46a
Lu <94569496+Lu47621@users.noreply.github.com>
2022-07-27 21:51:30 -0500

    Ejercicio 6. Funciones
    Ejercicio 6. Funciones
    Escribir una función que calcule el total de una factura tras aplicarle el IVA. 
    La función debe recibir dos cosas, la cantidad sin IVA y el porcentaje de IVA a aplicar, y devolver el total de la factura. 
    Si se invoca la función sin pasarle el porcentaje de IVA, deberá aplicar un 16%.

---
+++ Ejercicio 6 - Funciones.py
@@ -0,0 +1,10 @@
+# Escribir una función que calcule el total de una factura tras aplicarle el IVA. 
+# La función debe recibir dos cosas, la cantidad sin IVA y el porcentaje de IVA a 
+# aplicar, y devolver el total de la factura. 
+# Si se invoca la función sin pasarle el porcentaje de IVA, deberá aplicar un 16%
+
+def fac_iva(sin_iva, iva=16): #sin_iva --> la cantidad sin IVA        iva -->porcentaje de IVA
+    return sin_iva + sin_iva*iva/100
+
+print(fac_iva(1600,8))
+print(fac_iva(1600))
